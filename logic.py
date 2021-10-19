93647dcbe74cb887b3071ed76121625594471deb
Umair Dharejo <hackerbadshah726@gmail.com>
2021-10-19 22:33:51 +0500

    If directory is empty, it won't ask for extension

--- logic.py
+++ logic.py
@@ -8,3 +8,11 @@ else:
     sys.exit()
 if os.path.isfile(req_path):
     print(f"The given path {req_path} is a file. Please pass only directory path")
+
+else:
+    all_f_ds=os.listdir(req_path)
+    if len(all_f_ds)==0:
+        print(f"This given path is {req_path} is empty")
+    else:
+        req_ex=input("enter the required file extension with . : ")
+        req_files=[]
