bf6696191bef16ce43e42fe208e0b4ff450ee2bd
Umair Dharejo <hackerbadshah726@gmail.com>
2021-10-19 22:29:05 +0500

    added code to check whether given path is file

--- logic.py
+++ logic.py
@@ -6,3 +6,5 @@ if os.path.exists(req_path):
 else:
     "Please enter Valid Path"
     sys.exit()
+if os.path.isfile(req_path):
+    print(f"The given path {req_path} is a file. Please pass only directory path")
